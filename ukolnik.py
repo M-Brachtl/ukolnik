12c8d385afc90d93c80f474816d0cc777daab37b
Matej Brachtl <bra42176@spseol.cz>
2024-11-27 13:05:34 +0100

    Základní struktura kódu

---
+++ ukolnik.py
@@ -0,0 +1,42 @@
+import time
+print(time.localtime())
+print(time.asctime(time.localtime()))
+class Ukol:
+    def __init__(self,title,description,start_date,deadline,difficulty) -> None:
+        self.title = title
+        self.description = description
+        self.start_date = start_date
+        self.deadline = deadline
+        self.progress = 0
+        self.difficulty = difficulty
+    def move_deadline(self):
+        pass
+    def add_progress(self):
+        pass
+    def all_info(self):
+        return
+
+class Ukolnik:
+    def __init__(self,name) -> None:
+        self.name = name
+        self.ukoly = []
+        self.done = []
+        self.failed = []
+    def add_ukol(self,title,description,start_date,deadline,difficulty):
+        self.ukoly.append(Ukol(title,description,start_date,deadline,difficulty))
+    def delete_ukol(self,some_info):
+        pass
+    def load_from_JSON(self,filename):
+        pass
+    def list_ukoly(self,ordering = "abc"): # abc, dead(line), start, diff
+        return
+    def plot_it(self):
+        pass
+    def notification(self):
+        pass
+    def search(self,key):
+        return
+    
+# try loading Ukolniky from ukolnik.json
+# else create a new one
+# Ukolnik(input("Zadejte jméno svého prvního Úkolníku."))
